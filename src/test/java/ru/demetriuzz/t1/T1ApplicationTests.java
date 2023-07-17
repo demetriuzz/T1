2522b1edc8f7ef5dc8a5eb0d0fcb494f057df627
Dmitrii V. Lystsov <dlystsov@softmedialab.com>
2023-07-17 13:26:09 +0500

    uniform application.yaml for main and test

--- src/test/java/ru/demetriuzz/t1/T1ApplicationTests.java
+++ src/test/java/ru/demetriuzz/t1/T1ApplicationTests.java
@@ -2,8 +2,10 @@ package ru.demetriuzz.t1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class T1ApplicationTests {
 
     @Test
