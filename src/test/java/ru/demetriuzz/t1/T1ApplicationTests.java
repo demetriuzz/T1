aa652e3972ba38c551b173c7eca2efedfb73c921
Dmitrii V. Lystsov <dlystsov@softmedialab.com>
2023-07-19 15:21:48 +0500

    update testcontainers values

--- src/test/java/ru/demetriuzz/t1/T1ApplicationTests.java
+++ src/test/java/ru/demetriuzz/t1/T1ApplicationTests.java
@@ -3,10 +3,17 @@ package ru.demetriuzz.t1;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Testcontainers
 class T1ApplicationTests {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest");
 
     @Test
     void contextLoads() {
