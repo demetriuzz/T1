83d8f85eea7caa33cb44033df4a74b2d0188caa4
Dmitrii V. Lystsov <dlystsov@softmedialab.com>
2023-07-19 16:13:07 +0500

    add jooq test

--- src/test/java/ru/demetriuzz/t1/T1ApplicationTests.java
+++ src/test/java/ru/demetriuzz/t1/T1ApplicationTests.java
@@ -1,11 +1,20 @@
 package ru.demetriuzz.t1;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import ru.demetriuzz.t1.jooq.notes.t1_schema.tables.pojos.TNote;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -15,8 +24,16 @@ class T1ApplicationTests {
     @Container
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest");
 
+    @Autowired
+    private NoteDao repo;
+
     @Test
     void contextLoads() {
+        TNote note = new TNote(null, LocalDate.of(2023, 7, 1), LocalTime.of(10, 0, 0), "todo 1");
+        repo.insert(note);
+        assertThat(note.getCId()).isNotZero();
+
+        assertThat(repo.getNotes()).isEqualTo(List.of(note));
     }
 
 }
